119871409d7531b5ae798f143498de4a9e00682e
ed <ed@maidavale.org>
2020-03-18 11:31:36 +0000

    Update ping healthcheck endpoint test

---
+++ src/test/java/uk/gov/justice/digital/hmpps/interventionscatalogue/integration/PingEndpointTest.java
@@ -0,0 +1,20 @@
+package uk.gov.justice.digital.hmpps.interventionscatalogue.integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+class PingEndpointTest extends IntegrationTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    void getPingReturnsPong() throws Exception {
+        mockMvc.perform(get("/ping"))
+                .andExpect(content().string("pong"));
+    }
+}
